a3b93c7af3cc0654fc951e9e5a15ce246574a67b
Samuel Hawkins <Samuel.hawkins1998@gmail.com>
2023-10-25 16:36:26 -0400

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import Sidebar from '@/components/Sidebar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
+import SupabaseProvider from '@/providers/SupabaseProviders'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -18,10 +21,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <SupabaseProvider>
+        <UserProvider>
+        <ModalProvider/> 
         <Sidebar>
          {children}
         </Sidebar>
+        </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   )
 }
+
+// Modal Provider is a self closing component 

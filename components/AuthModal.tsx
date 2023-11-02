548a5b918d21e57761ecc36957fd030090099a08
Samuel Hawkins <Samuel.hawkins1998@gmail.com>
2023-11-02 17:35:14 -0400

    feat

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -4,7 +4,7 @@ import {
     useSessionContext 
        } from '@supabase/auth-helpers-react'
 import Modal from './Modal'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
@@ -17,7 +17,18 @@ const AuthModal = () => {
     const supabaseClient = useSupabaseClient();
     const router = useRouter();
     const {session} = useSessionContext();
-    const { onClose, isOpen} = useAuthModal(); 
+    const { onClose, isOpen} = useAuthModal();
+    
+    useEffect(() => {
+        if (session) {
+            router.refresh();
+            onClose();  
+        }
+    }, [session, router, onClose ])
+
+
+
+
     const onChange = (open: boolean) => { 
         if (!open) {
             onClose(); 
